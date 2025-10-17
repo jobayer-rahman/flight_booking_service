89bb8c8f9ed3a42e385c5d127459418a95faaace
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-18 01:48:26 +0600

    Added the User model.

--- backendSvc/users/models.py
+++ backendSvc/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = "admin", "Admin"
+        CUSTOMER = "customer", "Customer"
+        CARRIER = "carrier", "Carrier"
+        AGENCY = "agency", "Agency"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.CUSTOMER
+    )
+    msisdn = models.CharField(max_length=15)
+    is_banned = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = self.Roles.ADMIN
+        super().save(*args, **kwargs)
