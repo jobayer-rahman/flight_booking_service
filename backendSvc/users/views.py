5192753ea567fe4e0a0cd36a603db8c5cec427d3
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-20 02:22:00 +0600

    Added UserLogoutAPIView for logout api.

--- backendSvc/users/views.py
+++ backendSvc/users/views.py
@@ -3,16 +3,31 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework import views
 from rest_framework.response import Response
-from .serializers import UserRegistrationSerializer, LoginSerializer
+from .serializers import (
+    UserRegistrationSerializer,
+    LoginSerializer,
+    UserLogoutSerializer,
+)
 
 
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
 
-class LoginAPIView(views.APIView):
+class UserLoginAPIView(views.APIView):
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogoutAPIView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserLogoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"detail": "Logout successful"},
+                status=status.HTTP_200_OK
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
