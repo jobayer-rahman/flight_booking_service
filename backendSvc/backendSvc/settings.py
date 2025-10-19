2ed04403631d111cc57978fc37b7df5e04b1f8bf
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-20 02:17:49 +0600

    Added configuration for token_blacklist.

--- backendSvc/backendSvc/settings.py
+++ backendSvc/backendSvc/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'users.apps.UsersConfig',
 ]
 
@@ -66,7 +67,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
