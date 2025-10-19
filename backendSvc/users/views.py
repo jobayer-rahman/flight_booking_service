f507aeff799ae6d2e8a1176c015c8aee44be01f0
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-19 23:56:11 +0600

    Added LoginAPIView.

--- backendSvc/users/views.py
+++ backendSvc/users/views.py
@@ -1,8 +1,18 @@
-from rest_framework import generics
 from .models import User
-from .serializers import UserRegistrationSerializer
+from rest_framework import generics
+from rest_framework import status
+from rest_framework import views
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer, LoginSerializer
 
 
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+class LoginAPIView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
