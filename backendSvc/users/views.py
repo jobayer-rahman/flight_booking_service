ce8c67cf431284b1c254adba7f898b5899690e8e
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-21 20:51:03 +0600

    Added view for refresh-token api endpoint.

--- backendSvc/users/views.py
+++ backendSvc/users/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .serializers import (
     UserRegistrationSerializer,
     LoginSerializer,
+    RefreshTokenSerializer,
     UserLogoutSerializer,
 )
 
@@ -17,6 +18,13 @@ class UserRegisterView(generics.CreateAPIView):
 class UserLoginAPIView(views.APIView):
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RefreshTokenAPIView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RefreshTokenSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
