ddca7f010c79dc497edd9175d7f54a707d5868d6
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-21 20:50:14 +0600

    Added refresh-token endpoint.

--- backendSvc/users/urls.py
+++ backendSvc/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    RefreshTokenAPIView,
     UserRegisterView,
     UserLoginAPIView,
     UserLogoutAPIView
@@ -9,5 +10,6 @@ from .views import (
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginAPIView.as_view(), name='login'),
+    path('refresh-token/', RefreshTokenAPIView.as_view(), name='refresh-token'),
     path('logout/', UserLogoutAPIView.as_view(), name='logout'),
 ]
