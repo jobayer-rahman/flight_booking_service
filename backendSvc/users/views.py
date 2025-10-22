f386a465cbf42469b3451b190f984a4766ff8c97
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-22 20:14:09 +0600

    Updated the RefreshTokenAPIView.

--- backendSvc/users/views.py
+++ backendSvc/users/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework import views
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenRefreshView
 from .serializers import (
     UserRegistrationSerializer,
     LoginSerializer,
@@ -22,12 +23,8 @@ class UserLoginAPIView(views.APIView):
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class RefreshTokenAPIView(views.APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = RefreshTokenSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RefreshTokenAPIView(TokenRefreshView):
+    serializer_class = RefreshTokenSerializer
 
 class UserLogoutAPIView(views.APIView):
     def post(self, request, *args, **kwargs):
