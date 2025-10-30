2390b75365258c517b5b2008d725d9c756851a63
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-10-30 21:52:08 +0600

    Added flights app to the INSTALLED_APPS.

--- backendSvc/backendSvc/settings.py
+++ backendSvc/backendSvc/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'users.apps.UsersConfig',
+    'flights.apps.FlightsConfig',
 ]
 
 MIDDLEWARE = [
