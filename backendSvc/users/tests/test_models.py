f0625be7a578f438930aa7c204b3fa65be7dc0ec
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-11-01 03:09:43 +0600

    Refactored the test code for the User model. - Added UserFactory and SuperUserFactory for generating user instances. - Improved test readability and consistency across user model tests.

--- backendSvc/users/tests/test_models.py
+++ backendSvc/users/tests/test_models.py
@@ -1,52 +1,25 @@
 from ..models import User
 from django.test import TestCase
+from users.factories import SuperUserFactory, UserFactory
 
 class UserModelTests(TestCase):
     def test_default_customer_role(self):
-        data = {
-            "username": "test_user",
-            "password": "test@1234",
-            "msisdn": "123456"
-        }
-        user = User.objects.create_user(**data)
+        user = UserFactory()
         self.assertEqual(user.role, User.Roles.CUSTOMER)
 
     def test_superuser_role(self):
-        data = {
-            "username": "admin",
-            "password": "admin@1234",
-            "msisdn": "99999999"
-        }
-        superuser = User.objects.create_superuser(**data)
+        superuser = SuperUserFactory()
         self.assertEqual(superuser.role, User.Roles.ADMIN)
         self.assertEqual(superuser.is_superuser, True)
 
     def test_carrier_role(self):
-        data = {
-            "username": "carrier",
-            "password": "carrier@4321",
-            "msisdn": "34567890",
-            "role": "carrier"
-        }
-        carrier = User.objects.create_user(**data)
+        carrier = UserFactory(role="carrier")
         self.assertEqual(carrier.role, User.Roles.CARRIER)
 
     def test_admin_role(self):
-        data = {
-            "username": "admin",
-            "password": "admin@4321",
-            "msisdn": "34567890",
-            "role": "admin"
-        }
-        admin = User.objects.create_user(**data)
+        admin = UserFactory(role="admin")
         self.assertEqual(admin.role, User.Roles.ADMIN)
 
     def test_agency_role(self):
-        data = {
-            "username": "agency",
-            "password": "agency@4321",
-            "msisdn": "34567890",
-            "role": "agency"
-        }
-        agency = User.objects.create_user(**data)
+        agency = UserFactory(role="agency")
         self.assertEqual(agency.role, User.Roles.AGENCY)
